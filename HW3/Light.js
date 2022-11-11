0c1a656a650b658bab88dfb7abb3f072fa37c066
KingTa <096084@jyes.ntpc.edu.tw>
2022-11-12 07:23:41 +0800

    e

--- HW3/Light.js
+++ HW3/Light.js
@@ -1,5 +1,6 @@
 class Light{
 	constructor(){
+		//燈線
 		this.tipe = new THREE.Group();
 		
 		this.tipe1 = new THREE.Mesh(new THREE.RingGeometry(615, 620, 4, 6, Math.PI/4), new THREE.MeshLambertMaterial({color : 0x888888, side : THREE.DoubleSide}));
@@ -53,9 +54,28 @@ class Light{
 		this.tipe12.rotation.x = Math.PI/2;
 		
 		this.tipe.add(this.tipe1, this.tipe2, this.tipe3, this.tipe4, this.tipe5
-		, this.tipe6, /*this.tipe7, */this.tipe8, this.tipe9, this.tipe10, this.tipe11, this.tipe12);
-		
+		, this.tipe6, this.tipe7, this.tipe8, this.tipe9, this.tipe10, this.tipe11, this.tipe12);
 		
 		
 	}
+	
+	
+}
+
+function makelight(x, z, tx, tz){
+									//color 颜色, intensity 强度, distance 距离, angle 散射角, penumbra 衰减百分比, decay 衰减
+		const spotLight = new THREE.SpotLight(  0xffffff, 10, 150, 0.3, 0.25, 2);
+		spotLight.position.set( x, 145, z );
+		spotLight.castShadow = true; // 光源產生陰影
+		
+		const pointlight = new THREE.PointLight(0xFEF4B5, 0.1);
+		pointlight.position.set(x, 145, z);
+	
+		let target = new THREE.Object3D();
+		spotLight.target.position.set(tx, 80, tz);
+		scene.add(spotLight, spotLight.target, pointlight);
+		
+		const spotLightHelper = new THREE.SpotLightHelper( spotLight );
+		
+		//scene.add( spotLightHelper);
 }
